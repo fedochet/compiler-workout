3a28c95807a42c57b4aef85a7c7f64427d72d004
danyaberezun <danya.berezun@gmail.com>
2018-10-05 12:37:11 +0300

    fix test.sh

--- regression/test.sh
+++ regression/test.sh
@@ -1,3 +1,3 @@
-make check
-pushd expressions && make check && popd
-pushd deep-expressions && make check && popd
+make check &&
+    pushd expressions && make check && popd &&
+    pushd deep-expressions && make check && popd
